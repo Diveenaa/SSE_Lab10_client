89755365500451fa9db8e90a973e28290bd152ec
dn123 <diveena.nanthakumaran22@imperial.ac.uk>
2024-01-31 20:56:20 +0000

    amend filter books

--- app.py
+++ app.py
@@ -21,57 +21,47 @@ def request_books():
         books = response.json()
         return books
 
-@app.route('/books')
 def filter_books(genre=None, id=None, title=None, year=None,author=None):
-    args_list = list[genre, id, title, author, year]
-    if all(v is None for v in args_list):
-        genre = request.args.get('genre')
-        id = request.args.get('min_id')
-        title = request.args.get('title')
-        year = request.args.get('min_year')
-        author = request.args.get('author')
-
     books = request_books()
-    books = books["books"]
+    resulting_books = books = books["books"]
     if id:
-        resulting_books = [book for book in books if int(id) <= int(book['id'])]
-        if len(resulting_books) == 0:
-            return "No books found"
-        return resulting_books
+        resulting_books = [book for book in resulting_books if int(id) <= int(book['id'])]
 
     if title:
-        resulting_books = [book for book in books if title.lower() in book['title'].lower()]
-        if len(resulting_books) == 0:
-            return "No books found"
-        return resulting_books
+        resulting_books = [book for book in resulting_books if title.lower() in book['title'].lower()]
 
     if year:
-        resulting_books = [book for book in books if int(year) <= int(book['publication_year'])]
-        if len(resulting_books) == 0:
-            return "No books found"
-        return resulting_books
+        resulting_books = [book for book in resulting_books if int(year) <= int(book['publication_year'])]
 
     if author:
-        resulting_books = [book for book in books if author.lower() in book['author'].lower()]
-        if len(resulting_books) == 0:
-            return "No books found"
-        return resulting_books
+        resulting_books = [book for book in resulting_books if author.lower() in book['author'].lower()]
 
     if genre:
-        resulting_books = [book for book in books if genre.lower() in book['genre'].lower()]
-        if len(resulting_books) == 0:
-            return "No books found"
-        return resulting_books
+        resulting_books = [book for book in resulting_books if genre.lower() in book['genre'].lower()]
+
+    return resulting_books
+
+@app.route('/books')
+def filter_books_route():
+    genre = request.args.get('genre')
+    book_id = request.args.get('min_id')
+    title = request.args.get('title')
+    year = request.args.get('min_year')
+    author = request.args.get('author')
+
+    resulting_books = filter_books(genre, book_id, title, year, author)
+
+    if len(resulting_books) == 0:
+        return "No books found"
     
-    else:
-        return "Error"
-        #return request_books()
+    return resulting_books
 
 @app.route('/form_query', methods = ["POST"])
 def get_form_input():
     query = request.form.get('searchType')
     text_input = request.form.get('searchInput')
     print(query)
+    print(text_input)
     if query == 'genre':
         return filter_books(genre=text_input)
     if query == 'min_id':
